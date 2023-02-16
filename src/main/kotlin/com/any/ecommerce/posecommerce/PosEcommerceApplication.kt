b2f10caf3b528be931f6090990e70f693d0baf45
JovanCai <hit.caijunwen@gmail.com>
2023-02-16 19:22:03 +0900

    add init files

---
+++ src/main/kotlin/com/any/ecommerce/posecommerce/PosEcommerceApplication.kt
@@ -0,0 +1,11 @@
+package com.any.ecommerce.posecommerce
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PosEcommerceApplication
+
+fun main(args: Array<String>) {
+	runApplication<PosEcommerceApplication>(*args)
+}
---
+++ src/test/kotlin/com/any/ecommerce/posecommerce/PosEcommerceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.any.ecommerce.posecommerce
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PosEcommerceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
